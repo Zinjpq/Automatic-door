fd727b1fd6191dbee07d475c3e47224203efdd04
Zinjpq <quangvinh07032003@gmail.com>
2024-09-12 20:07:25 +0700

    Refactor code to add a new file test_request.py and main\1440x1024.py for ESP32-CAM object detection and identification

---
+++ main/1440x1024.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Tạo cửa sổ chính
+root = tk.Tk()
+
+# Đặt tiêu đề cho cửa sổ
+root.title("Cửa sổ 1440 x 1024")
+
+# Đặt kích thước cửa sổ là 1440x1024
+root.geometry("1440x1024")
+
+# Chạy vòng lặp chính để hiển thị cửa sổ
+root.mainloop()
