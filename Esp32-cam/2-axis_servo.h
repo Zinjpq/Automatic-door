f6bde78e732b9c818d2c13fce80b61897a8b1b1d
Zinjpq <quangvinh07032003@gmail.com>
2024-09-21 09:11:40 +0700

    fix day 2 :>

--- Esp32-cam/2-axis_servo.h
+++ Esp32-cam/2-axis_servo.h
@@ -1,25 +1,27 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
+#include <WebServer.h>
 
-int panAngle, tiltAngle;
+// // Servo object instances
+Servo panServo;
+Servo tiltServo;
+
+extern int panAngle;
+extern int tiltAngle;
 
 #define stepSize 10
 #define MIN_ANGLE 0
 #define MAX_ANGLE 180
 
-// Servo object instances
-Servo panServo;
-Servo tiltServo;
-
 // Define pins
 #define PAN_PIN 12
 #define TILT_PIN 13
 
-// // Functions to move the servos
-// void go_left(){
-//   panAngle = min(panAngle - stepSize, MIN_ANGLE);  
-//   panServo.write(panAngle);
-// }
+// Functions to move the servos
+void go_left(){
+  panAngle = min(panAngle - stepSize, MIN_ANGLE);  
+  panServo.write(panAngle);
+}
 
 // void go_right(){
 //   panAngle = max(panAngle + stepSize, MAX_ANGLE);  
@@ -41,16 +43,14 @@ void setup_2servo(){
   panServo.attach(PAN_PIN);
   tiltServo.attach(TILT_PIN);
 
-  panAngle = 90;
-  tiltAngle = 90;
-
   // Move servos to the starting position
   panServo.write(panAngle);
   tiltServo.write(tiltAngle);
 
   // Correct usage of server.on by passing function pointers
-  server.on("/left", panServo.write(min(panAngle - stepSize, MIN_ANGLE)));
-  server.on("/right", panServo.write(max(panAngle + stepSize, MAX_ANGLE)));
-  server.on("/up",tiltServo.write(min(tiltAngle - stepSize, MIN_ANGLE)));
-  server.on("/down", tiltServo.write(max(tiltAngle + stepSize, MAX_ANGLE));
+  server.on("/left", go_left);
+  // server.on("/right", panServo.write(max(panAngle + stepSize, MAX_ANGLE)));
+  // server.on("/up",    tiltServo.write(min(tiltAngle - stepSize, MIN_ANGLE)));
+  // server.on("/down",  tiltServo.write(max(tiltAngle + stepSize, MAX_ANGLE)));
+
 }
--- Esp32-cam/esp32-cam.ino
+++ Esp32-cam/esp32-cam.ino
@@ -9,8 +9,6 @@
 #include "cam.h"
 #include "2-axis_servo.h"
 
-
-
 void setup() {
   Serial.begin(115200);
 
