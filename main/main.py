baac5d5660949b62625be66fffc6754651d2d35c
Zinjpq <quangvinh07032003@gmail.com>
2024-09-30 15:00:07 +0700

    Refactor code to remove unused imports and print statements

--- main/main.py
+++ main/main.py
@@ -1,8 +1,1 @@
 # Main.py
-import library
-import DetectChars
-
-if __name__ == "__main__":
-    print("Hello, this is main program")
-    print(library.check_plate("29B 12345"))
-    print(DetectChars.loadKNNDataAndTrainKNN())
