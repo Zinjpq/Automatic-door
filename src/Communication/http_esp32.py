40b5473dc684bb4461f0298c5117771f9c7c63cc
Zinjpq <quangvinh07032003@gmail.com>
2025-04-23 17:59:47 +0700

    Update http_esp32.py

--- src/Communication/http_esp32.py
+++ src/Communication/http_esp32.py
@@ -1,10 +1,11 @@
 import requests
 
-esp32_ip = "http://192.168.1.xxx"  # IP ESP32
-pan_angle = 90
-tilt_angle = 45
+esp32_ip = "http://192.168.4.1"  # IP ESP32
+pan_angle = 40
+tilt_angle = 90
+door_status = 1
 
-response = requests.get(f"{esp32_ip}/set_angle", params={"pan": pan_angle, "tilt": tilt_angle})
+response = requests.get(f"{esp32_ip}/set_angle", params={"pan": pan_angle, "tilt": tilt_angle, "door": door_status})
 print(response.text)
 
 # ESP32-CAM URL and Control URLs
@@ -12,18 +13,14 @@ ESP32_BASE_URL = 'http://192.168.4.184'
 CAMERA_URL = ESP32_BASE_URL + '/cam'
 RasPi_Base_URL = "http://192.168.4.2:5000"
 
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/door_status", methods=["POST"])
-def door_status():
-    status = request.json.get("status")
-    print(f"Cửa hiện tại: {status}")
-    return "OK"
-
-
-
-
-
-app.run(host="0.0.0.0", port=5000)
+# from flask import Flask, request
+#
+# app = Flask(__name__)
+#
+# @app.route("/door_status", methods=["POST"])
+# def door_status():
+#     status = request.json.get("status")
+#     print(f"Cửa hiện tại: {status}")
+#     return "OK"
+#
+# app.run(host="0.0.0.0", port=5000)
